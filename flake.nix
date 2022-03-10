3fee67fccd536c11518de35a918ec83f74b123b8
Julius de Bruijn <julius+github@nauk.io>
2022-03-10 17:30:08 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A prisma test project";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          dotnet-sdk
+        ];
+        PATH = ''$PATH:/home/pimeys/.dotnet/tools'';
+        DOTNET_ROOT = pkgs.dotnet-sdk;
+      };
+    });
+}
